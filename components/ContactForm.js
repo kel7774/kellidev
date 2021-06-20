d1d8c15e89beac92e99f4fa445bac027f8670a74
Kelli Landry <landry.kel@gmail.com>
2021-06-20 16:24:54 -0500

    use sendgrid model with api, not sending. todo: look at input being sent

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,55 +1,44 @@
-import { useForm } from 'react-hook-form'
 import { useState } from 'react'
 import styles from '../styles/ContactForm.module.css'
-import { reset } from 'axe-core'
 
 export default function ContactForm () {
-  const { register, handleSubmit, formState: { errors } } = useForm()
-  const [firstName, setFirstName] = useState(undefined)
-  const [lastName, setLastName] = useState(undefined)
-  const [email, setEmail] = useState(undefined)
-  const [message, setMessage] = useState(undefined)
-
-  function handleFirstNameChange (e) {
-    setFirstName(e.target.value)
-  }
-  function handleLastNameChange (e) {
-    setLastName(e.target.value)
-  }
-  function handleEmailChange (e) {
-    setEmail(e.target.value)
+  const [state, setState] = useState({ firstName: '', lastName: '', email: '', message: '' })
+  const handleChange = event => {
+    const { name, value } = event.target
+    setState({
+      ...state,
+      [name]: value
+    })
   }
 
-  function handleMessageChange (e) {
-    setMessage(e.target.value)
-  }
-
-  const sendMail = async (data) => {
-    try {
-      await fetch('../pages/api/email.ts', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(data)
+  const handlePress = () => {
+    fetch ('/api/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: state.firstName,
+        lastName: state.lastName,
+        email: state.email,
+        message: state.message
       })
-      setTimeout(() => reset(), 2000)
-    } catch (error) {
-      console.log(error)
-    }
+    })
   }
-
   return (
-    <form className='contactForm' onSubmit={handleSubmit(sendMail)}>
+    <form className='contactForm' onSubmit={handlePress}>
       <div className={styles.formField}>
         <label htmlFor='firstName'>first name</label>
         <input
           type='text'
           name='firstName'
           placeholder='first name'
-          value={firstName}
-          onChange={() => handleFirstNameChange}
-          {...register('firstName', { required: true })}
+          onChange={handleChange}
+          // value={firstName}
+          // onChange={() => handleFirstNameChange}
+          // {...register('firstName', { required: true })}
         />
-        {errors.firstName && 'first name is required'}
+        {/* {errors.firstName && 'first name is required'} */}
       </div>
       <div className={styles.formField}>
         <label htmlFor='lastName'>last name</label>
@@ -57,11 +46,12 @@ export default function ContactForm () {
           type='text'
           name='lastName'
           placeholder='last name'
-          value={lastName}
-          onChange={() => handleLastNameChange}
-          {...register('lastName', { required: true })}
+          onChange={handleChange}
+          // value={lastName}
+          // onChange={() => handleLastNameChange}
+          // {...register('lastName', { required: true })}
         />
-        {errors.lastName && 'last name is required'}
+        {/* {errors.lastName && 'last name is required'} */}
       </div>
       <div className={styles.formField}>
         <label htmlFor='email'>email</label>
@@ -69,17 +59,18 @@ export default function ContactForm () {
           type='email'
           name='email'
           placeholder='email'
-          value={email}
-          onChange={() => handleEmailChange}
-          {...register('email', {
-            required: 'required',
-            pattern: {
-              value: /\S+@\S+.\S+/,
-              message: 'must be a valid email'
-            }
-          })}
+          onChange={handleChange}
+          // value={email}
+          // onChange={() => handleEmailChange}
+          // {...register('email', {
+          //   required: 'required',
+          //   pattern: {
+          //     value: /\S+@\S+.\S+/,
+          //     message: 'must be a valid email'
+          //   }
+          // })}
         />
-        {errors.email && 'email is required'}
+        {/* {errors.email && 'email is required'} */}
       </div>
       <div className={styles.formField}>
         <label htmlFor='message'>message</label>
@@ -87,14 +78,14 @@ export default function ContactForm () {
           placeholder='message ...'
           className={styles.textarea}
           name='message'
-          value={message}
-          onChange={() => handleMessageChange}
-          {...register('message', {
-            required: 'message required',
-            minLength: { value: 100, message: 'Must enter at least 100 characters' }
-          })}
+          // value={message}
+          // onChange={() => handleMessageChange}
+          // {...register('message', {
+          //   required: 'message required',
+          //   minLength: { value: 100, message: 'Must enter at least 100 characters' }
+          // })}
         />
-        {errors.textarea && 'a message of at least 100 characters is required'}
+        {/* {errors.textarea && 'a message of at least 100 characters is required'} */}
       </div>
       <input type='submit' className={styles.submit} />
     </form>
