540de13117e5dbcfcf370a334a4d763a7013e064
Kelli Landry <kellilandry@Kellis-MacBook-Pro.local>
2022-07-22 20:00:05 -0500

    make profile pic smaller for screens, too big

--- components/MainContent.js
+++ components/MainContent.js
@@ -6,18 +6,18 @@ export default function MainContent () {
       <header>
         <div className='flex flex-col text-right px-3'>
           <h1 className='font-light text-8xl lowercase dark:text-indigo-100'>Kelli Landry</h1>
-          <h2 className='text-4xl lowercase dark:text-indigo-100'>junior software developer, scrum master</h2>
+          <h2 className='text-4xl lowercase dark:text-indigo-100'>software developer, scrum master</h2>
           <h3 className='text-2xl lowercase dark:text-indigo-100'><span>📍</span>lafayette, la</h3>
         </div>
       </header>
-      <div className='xs:p-7 sm:p-4 md:p-4 lg:p-4 xl:p-4 2xl:p-4'>
-        <Image
-          src='/New_Mexico_Overlay.png'
+      <div className='w-full flex justify-center p-7 profilePicBox'>
+        <Image 
+          src='/New_Mexico_Overlay.png' 
+          alt='Kelli Landry Main' 
+          height='600'
           width='500'
-          height='500'
-          layout='responsive'
-          alt='Picture of Kelli in New Mexico'
-          className='rounded-full'
+          objectFit='cover' 
+          className='rounded-lg'
         />
       </div>
     </section>
