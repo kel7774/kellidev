5f9e08b3e72392adfa6157b5d0baf049e6884cde
Kelli Landry <landry.kel@gmail.com>
2025-10-18 18:45:56 -0500

    add full range of services to services api, create custom functions for each button toggle

--- pages/api/services.js
+++ pages/api/services.js
@@ -1,8 +1,198 @@
 export const services = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
+    {
+        id: 1,
+        name: 'Career',
+        offers: [
+            'Resume review, proofread, and optimazation for ATS compatibility',
+            'LinkedIn profile review and optimization',
+            'Cover letter template creation',
+            'Job board onboarding, navigation assistance, and profile optimization',
+            'Networking stragtegy and outreach guidance',
+            'Tech specific: introduction to coding bootcamps ranging from software developer to leadership roles',
+            'Spreadsheet tracking template for job applications and follow-ups',
+        ],
+    },
+    {
+        id: 2,
+        name: 'Home',
+        offers: [
+            'Organizing closets, cabinets, and storage spaces',
+            'Decluttering and donating unwanted items',
+            'Deep cleaning - dusting, sweeping, mopping, vacuuming, disinfecting ceiling to floor',
+            'Washing windows and mirrors',
+            'Moving assistance - packing, loading, unloading, unpacking, organizing',
+            'Assisting with home renovation projects',
+            'Doing and folding laundry, including ironing and steaming clothes',
+            'Creating cleaning schedules and checklists',
+            'Coordinating with contractors and service providers',
+            'Light yard work such as weeding, planting flowers, and general tidying',
+            'Assembling furniture and home decor items',
+            'Hanging up pictures, shelves, curtains, and other wall-mounted items',
+            'Setting up smart home devices and systems - TVs, speakers, lights, projectors, Roku, Amazon Echo & Alexa, Firesticks, Google Home, ect',
+            'Eco-friendly home tips and product recommendations',
+            'House sitting services including plant care, mail collection, pet care (see Pet section)',
+        ],
+    },
+    {
+        id: 3,
+        name: 'Pets',
+        offers: [
+            'Dog walking and exercise',
+            'Pet sitting services including feeding, watering, and companionship',
+            'Grooming assistance such as brushing & bathing',
+            'Organizing pet supplies and creating storage solutions',
+            'Scheduling vet appointments and managing pet health records',
+            'Assisting with pet adoption research and processes',
+            'Transportation to and from vet appointments or grooming sessions',
+        ],
+    },
+    {
+        id: 4,
+        name: 'Childcare',
+        offers: [
+            'Babysitting services for children of various ages',
+            'Assisting with homework and educational activities',
+            'Organizing playdates and social activities',
+            'Creating schedules and routines for children',
+            'Preparing simple meals and snacks',
+            'Engaging in creative activities such as arts and crafts, reading, and outdoor play',
+            'Light housekeeping related to childcare, such as tidying up toys and cleaning up after meals',
+        ]
+    },
+    {
+        id: 5,
+        name: 'Technology',
+        offers: [
+            'Website audit and recommendations for improvement',
+            'Tech tool recommendations for small businesses or personal use',
+            'Basic troubleshooting for common tech issues',
+            'Guidance on setting up productivity tools and apps',
+            'Cleaning up email inboxes & unsubscribing from unwanted newsletters',
+            'Sending emails, setting up automations, and managing contacts',
+            'Organizing digital files and folders for easy access',
+            'Organizing social media accounts, blocking unwanted followers, and updating bios',
+            'Phone setup and optimization, including app recommendations and organization',
+            'Switching from Android to iPhone or vice versa',
+            'Installing passwords managers and setting up secure passwords',
+        ],
+    },
+    {
+        id: 6,
+        name: 'Travel',
+        offers: [
+            'Researching destinations based on your interests and budget',
+            'Finding the best flight, car rental, and hotel options and deals',
+            'Recommending accommodations that suit your preferences',
+            'Creating a detailed itinerary with activities and dining options',
+            'Providing packing lists tailored to your destination and activities',
+            'Offering travel tips and advice for a smooth trip',
+            'Driving to airport(s) (limited to 4 people) or scheduling rideshares'
+        ],
+    },
+    {
+        id: 7,
+        name: 'Weddings',
+        offers:[
+            'Researching venues, vendors, and services based on your preferences and budget',
+            'Creating checklists for 1 month, 3 months, 6 months, and 1 year before the wedding',
+            'Creating a detailed wedding day timeline and checklist',
+            'Coordinating with vendors and managing communication',
+            'Assisting with guest list management and RSVP tracking',
+            'Providing recommendations for wedding themes, decor, and activities',
+            'Offering tips for stress-free wedding planning and execution',
+            'Scheduling honeymoon, bachelor, and bachelorette trips (see Travel section for details)',
+            'Helping with flower arrangements and decorations',
+            'Heping with wedding favors and invitations',
+        ]
+    },
+    {
+        id: 8,
+        name: 'Money',
+        offers: [
+            'Budget creation and management, snyncing up with bank accounts',
+            'Cashapp, Venmo, PayPal, Zelle setup and optimization',
+            'Expense tracking and categorization',
+            'Savings goal setting and progress tracking',
+            'Bill payment reminders and organization',
+            'Organizing paper financial documents and statements',
+        ],
+    },
+    {
+        id: 9,
+        name: 'Mail & Packages',
+        offers: [
+            'Organizing and sorting physical mail and packages',
+            'Assisting with online order tracking and management',
+            'Coordinating package pickups and drop-offs',
+            'Helping with returns and exchanges for online purchases',
+            'Setting up notifications for important mail and package deliveries',]
+    },
+    {
+        id: 10,
+        name: 'Fitness & Wellness',
+        offers: [
+            'Recommending wellness apps and tools for tracking progress',
+            'Setting up accounts on fitness platforms and apps',
+            'Researching local fitness classes and wellness events',
+            'Researching gym memberships and personal trainers based on your goals and budget',
+            'Cancelling gym memberships',
+            'Compiliing healthy recipes and meal planning ideas',
+            'I can swim! Only freestyle stroke lessons available though :)',
+        ]
+    },
+    {
+        id: 11,
+        name: 'Car',
+        offers:[
+            'Scheduling regular maintenance appointments (oil changes, tire rotations, etc.)',
+            'Researching and recommending local mechanics and service centers',
+            'Organizing car documents and maintenance records',
+            'Setting up reminders for registration renewals and inspections',
+            'Assisting with car insurance research and comparisons',
+            'Bringing to drive through car washes or detail appointments',
+            'Washing car exterior, cleaning and vacuming interior',
+        ]
+    },
+    {
+        id: 12,
+        name: 'Medical',
+        offers:[
+            'Scheduling appointments with doctors, dentists, and specialists',
+            'Organizing medical records and documents',
+            'Transportation to and from medical appointments)',
+            'Setting up reminders for medication refills and appointments',
+            'Picking up prescriptions from local pharmacies',
+            'Calling insurance companies to verify coverage and benefits (and argue, who knows)',
+            'Researching healthcare providers and facilities based on your needs and location',
+            'Assisting with insurance paperwork and claims',
+            'Providing information on local health resources and support groups',
+        ]
+    },
+    {   id: 13,
+        name: 'Small Businesses',
+        offers:[
+            'Social media account setup and optimization',
+            'Researching industry trends and competitors, and creating competitor analysis reports',
+            'Assisting with customer communication and support',
+            'Organizing digital files and documents',
+            'Setting up productivity tools and apps for team collaboration',
+            'Basic bookkeeping and expense tracking',
+            'Creating templates for invoices, contracts, and other business documents',
+            'Shipping and receiving assistance for online orders',
+            'Process improvement recommendations and implementation',
+            'Shopify store setup and optimization',
+        ]
+    },
+    {
+        id: 14,
+        name: 'Other/Miscellaneous',
+        offers:[
+            'Event planning and coordination for birthdays, anniversaries, and other special occasions',
+            'Gift research and purchasing based on recipient preferences and budget',
+            'Grocery shopping or pickup, meal prep assistance, and pantry organization',
+            'Assisting with personal projects such as scrapbooking, photo organization, or crafting',
+            'Organizing garage sales or online selling of unwanted items',
+            'Returning library books or other borrowed items',
+        ]
+    }
 ]
